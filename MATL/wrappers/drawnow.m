55a847f4a2609ebe7af15219e7a197a40c530495
Jonathan Suever <suever@gmail.com>
2016-06-17 19:07:23 -0400

    Updates overloaded drawnow to actually save an image
    The image is saved to a temporary file and the location of this temporary file is printed to STDOUT (to be parsed later)

--- MATL/wrappers/drawnow.m
+++ MATL/wrappers/drawnow.m
@@ -1,3 +1,23 @@
 function varargout = drawnow(varargin)
-    [varargout{1:nargout}] = builtin('drawnow', varargin{:});
+
+    if get(gcf(), 'UserData')
+        [varargout{1:nargout}] = builtin('drawnow', varargin{:});
+    else
+        set(gcf(), 'UserData', 1)
+
+        base = 'image';
+
+        fname = @(x)[base, num2str(x), '.png'];
+
+        k = 0;
+        while exist(fname(k), 'file')
+            k = k + 1;
+        end
+
+        filename = fname(k);
+        disp(['[IMAGE]', filename])
+        print(gcf(), filename, '-dpng', '-tight')
+
+        set(gcf(), 'UserData', 0)
+    end
 end
