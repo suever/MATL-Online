bf6dd52a03e4b9d3154777b1eec0c4016783d640
Jonathan Suever <suever@gmail.com>
2016-07-03 09:35:28 -0400

    Removes custom error handlers for now since we don't have the HTML implemented yet

--- matl_online/app.py
+++ matl_online/app.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """The app module, containing the app factory function."""
-from flask import Flask, render_template
+from flask import Flask
 
 from matl_online import public
 from matl_online.assets import assets
@@ -18,7 +18,7 @@ def create_app(config_object=ProdConfig):
     app.config.from_object(config_object)
     register_extensions(app)
     register_blueprints(app)
-    register_errorhandlers(app)
+    #register_errorhandlers(app)
     return app
 
 
@@ -34,16 +34,4 @@ def register_extensions(app):
 def register_blueprints(app):
     """Register Flask blueprints."""
     app.register_blueprint(public.views.blueprint)
-    return None
-
-
-def register_errorhandlers(app):
-    """Register error handlers."""
-    def render_error(error):
-        """Render error template."""
-        # If a HTTPException, pull the `code` attribute; default to 500
-        error_code = getattr(error, 'code', 500)
-        return render_template('{0}.html'.format(error_code)), error_code
-    for errcode in [401, 404, 500]:
-        app.errorhandler(errcode)(render_error)
     return None
