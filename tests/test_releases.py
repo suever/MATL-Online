62e645daf0c30638863698da5ae5e7bb2ca8a832
Jonathan Suever <suever@gmail.com>
2016-07-16 17:31:25 -0400

    Adds a few more tests to releases

--- tests/test_releases.py
+++ tests/test_releases.py
@@ -17,10 +17,33 @@ class TestRelease:
             assert ReleaseFactory() == Release.latest()
 
         # Now add a new entry that has an old date
-        release = ReleaseFactory(date=datetime(2000, 1, 1))
+        release = Release.create(date=datetime(2000,1,1), tag='1.2.3')
 
         # Make sure that this is NOT the latest release
         assert release != Release.latest()
+
+    def test_remove_release(self):
+        # Make sure that we can delete a release if needed
+        nRelease = 10
+        ReleaseFactory.create_batch(size=nRelease)
+
+        releases = Release.query.all()
+
+        assert len(releases) == nRelease
+
+        # Remove the last release
+        last_release = releases[-1]
+        last_release.delete()
+
+        releases = Release.query.all()
+
+        assert len(releases) == nRelease - 1
+        assert Release.query.filter_by(tag=last_release.tag).all() == []
+
+    def test_release_repr(self):
+        # Check that the release is displayed properly if coerced
+        release = ReleaseFactory(tag='1.2.3')
+        assert release.__repr__() == '<Release %r>' % '1.2.3'
 
     def test_factory(self, db):
         """
