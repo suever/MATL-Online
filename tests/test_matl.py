2ea0510079ea35309e50e720d4ddb7161301074b
Jonathan Suever <suever@gmail.com>
2016-07-17 15:16:22 -0400

    Adds test methods for converting help.mat to json

--- tests/test_matl.py
+++ tests/test_matl.py
@@ -1,7 +1,11 @@
 import base64
+import json
 import os
+import shutil
 
-from matl_online.matl import parse_matl_results, get_matl_folder
+from matl_online.matl import parse_matl_results, get_matl_folder, help_file
+
+TEST_DATA_DIR = os.path.join(os.path.dirname(__file__), 'data')
 
 
 class TestSourceCache:
@@ -99,3 +103,66 @@ class TestResults:
         assert len(result) == 1
         assert result[0]['type'] == 'stdout'
         assert result[0]['value'] == expected
+
+
+class TestHelpParsing:
+
+    def test_generate_help_json(self, tmpdir, mocker):
+
+        folder = mocker.patch('matl_online.matl.get_matl_folder')
+        folder.return_value = tmpdir.strpath
+
+        # Copy the test file into place
+        shutil.copy(os.path.join(TEST_DATA_DIR, 'help.mat'),
+                    os.path.join(tmpdir.strpath, 'help.mat'))
+
+        outfile = help_file('1.2.3')
+
+        assert outfile == os.path.join(folder.return_value, 'help.json')
+
+        # Now actually check the file
+        with open(outfile, 'r') as fid:
+            data = json.load(fid)
+
+        assert 'data' in data
+        assert len(data['data']) == 2
+
+        # Make sure it has all the necessary keys
+        expected = ['source', 'description', 'brief', 'arguments']
+        expected.sort()
+
+        actual = data['data'][0].keys()
+        actual.sort()
+
+        assert actual == expected
+
+        item = data['data'][0]
+
+        # make sure all newlines were removed from description
+        assert item.get('description').find('\n') == -1
+        assert item.get('arguments') == ''
+        assert item.get('source') == '<strong>&</strong>'
+        assert item.get('brief') == 'alternative input/output specification'
+
+        item = data['data'][1]
+
+        assert item.get('description').find('\n') == -1
+        assert item.get('arguments') == '1--2 (1 / 2);  1'
+        assert item.get('source') == '<strong>a</strong>'
+        assert item.get('brief') == 'any'
+
+    def test_help_json_exists(self, tmpdir, mocker):
+        folder = mocker.patch('matl_online.matl.get_matl_folder')
+        folder.return_value = tmpdir.strpath
+
+        jsonfile = tmpdir.join('help.json')
+        contents = 'placeholder'
+        jsonfile.write(contents)
+
+        outfile = help_file('1.2.3')
+
+        assert outfile == jsonfile.strpath
+
+        # Make sure the file wasn't updated
+        with open(outfile, 'r') as fid:
+            assert fid.read() == contents
