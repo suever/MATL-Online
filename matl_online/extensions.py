f5d091389a871195fc59a2d003b561a7942e8cce
Jonathan Suever <jonathan.suever@rigor.com>
2017-09-30 14:09:33 -0400

    Replace deprecated CsrfProtect with CSRFProtect

--- matl_online/extensions.py
+++ matl_online/extensions.py
@@ -3,7 +3,7 @@
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from flask_socketio import SocketIO
-from flask_wtf import CsrfProtect
+from flask_wtf import CSRFProtect
 
 from celery import Celery
 from matl_online.settings import Config
@@ -16,4 +16,4 @@ db = SQLAlchemy()
 migrate = Migrate()
 socketio = SocketIO()
 celery = Celery(__name__, broker=Config.CELERY_BROKER_URL)
-csrf = CsrfProtect()
+csrf = CSRFProtect()
