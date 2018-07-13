f6dcc9f91c8471516a7964e8a3873c77f77df741
Jonathan Suever <suever@gmail.com>
2018-07-13 10:06:33 -0400

    PEP8 cleanup

--- wsgi.py
+++ wsgi.py
@@ -1,5 +1,8 @@
+"""Script for initializing a production app for use with uwsgi."""
+
 import eventlet
 eventlet.monkey_patch()
+
 from matl_online.app import create_app
 from matl_online.settings import ProdConfig
 
