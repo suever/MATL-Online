33b3cfd0b7d3dfa1f22fd8a69112a0ba271c9f25
Jonathan Suever <suever@gmail.com>
2021-02-07 13:19:45 -0500

    Add missing docstrings

--- tests/helpers.py
+++ tests/helpers.py
@@ -1,6 +1,9 @@
+"""Functions to be used across tests for convenience."""
+
 from typing import Optional
 from flask_socketio import SocketIOTestClient
 
 
 def session_id_for_client(client: SocketIOTestClient, namespace: str = '/') -> Optional[str]:
+    """Retrieve the session id for a SocketIOTestClient."""
     return client.socketio.server.manager.sid_from_eio_sid(client.eio_sid, namespace)
