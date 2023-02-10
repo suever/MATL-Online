11ecd2a5a78984bf14368bf61832fe73d283b28a
Jonathan Suever <suever@gmail.com>
2023-02-10 10:17:28 -0500

    Add tests for the commands module

---
+++ tests/test_commands.py
@@ -0,0 +1,14 @@
+from flask import Flask
+from pytest_mock.plugin import MockerFixture
+
+
+def test_refresh_releases_command(mocker: MockerFixture, app: Flask) -> None:
+    # Mock out the actual refresh_releases function
+    refresh_releases = mocker.patch("matl_online.commands.matl.refresh_releases")
+
+    # Use the CLI to invoke the command
+    runner = app.test_cli_runner()
+    runner.invoke(args=["refresh_releases"])
+
+    # And ensure it had the intended behavior
+    refresh_releases.assert_called_once()
