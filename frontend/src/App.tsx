9483cb64dfae1f839d139a872800af59ed4eaf23
Jonathan Suever <suever@gmail.com>
2023-02-21 09:53:34 -0500

    WIP

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { useState } from 'react'
+import {useState} from 'react'
 import AppBar from '@mui/material/AppBar'
 import Box from '@mui/material/Box'
 import Toolbar from '@mui/material/Toolbar'
@@ -23,40 +23,46 @@ import Button from '@mui/material/Button'
 import HistoryIcon from '@mui/icons-material/History'
 import ShareIcon from '@mui/icons-material/Share'
 import CircularProgress from '@mui/material/CircularProgress'
+import LinearProgress from '@mui/material/LinearProgress'
+import PlayArrowIcon from '@mui/icons-material/PlayArrow'
+import StopIcon from '@mui/icons-material/Stop'
+import SchoolIcon from '@mui/icons-material/School'
+import HelpIcon from '@mui/icons-material/Help'
 
 const drawerWidth = 240
 
 function ButtonAppBar() {
+  const [open, setOpen] = useState<boolean>(true)
   return (
     <>
       <CssBaseline/>
-      <AppBar position="fixed" sx={{ zIndex: (theme) => theme.zIndex.drawer + 1}}>
+      <AppBar position="fixed" sx={{zIndex: (theme) => theme.zIndex.drawer + 1}}>
         <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{mr: 2}}
-          >
-            <MenuIcon/>
-          </IconButton>
+          <div onClick={() => setOpen(!open)}>
+            <IconButton
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              sx={{mr: 2}}
+            >
+              <MenuIcon/>
+            </IconButton>
+          </div>
           <Typography variant="h6" component="div" sx={{flexGrow: 1}}>
-            MATL Online
+                        MATL Online
           </Typography>
-          <TextField variant="outlined" placeholder="Search"> </TextField>
         </Toolbar>
       </AppBar>
       <Drawer
-        open={false}
-        variant="permanent"
+        variant='permanent'
         sx={{
           width: drawerWidth,
           flexShrink: 0
         }}
       >
         <Toolbar/>
-        <Box sx={{ overflow: 'auto', width: drawerWidth }}>
+        <Box sx={{overflow: 'auto', width: drawerWidth}}>
           <List>
             <ListItem key={'Interpreter'} disablePadding>
               <ListItemButton>
@@ -64,7 +70,7 @@ function ButtonAppBar() {
                   <CodeIcon/>
                 </ListItemIcon>
                 <ListItemText primary={'Interpreter'}>
-                  Interpreter
+                                    Interpreter
                 </ListItemText>
               </ListItemButton>
             </ListItem>
@@ -74,7 +80,17 @@ function ButtonAppBar() {
                   <MenuBookIcon/>
                 </ListItemIcon>
                 <ListItemText primary={'Documentation'}>
-                  Documentation
+                                    Documentation
+                </ListItemText>
+              </ListItemButton>
+            </ListItem>
+            <ListItem key={'Examples'} disablePadding>
+              <ListItemButton>
+                <ListItemIcon>
+                  <SchoolIcon/>
+                </ListItemIcon>
+                <ListItemText primary={'Examples'}>
+                                    Examples
                 </ListItemText>
               </ListItemButton>
             </ListItem>
@@ -84,7 +100,7 @@ function ButtonAppBar() {
                   <HistoryIcon/>
                 </ListItemIcon>
                 <ListItemText primary={'History'}>
-                  History
+                                    History
                 </ListItemText>
               </ListItemButton>
             </ListItem>
@@ -94,77 +110,115 @@ function ButtonAppBar() {
                   <BarChartIcon/>
                 </ListItemIcon>
                 <ListItemText primary={'Analytics'}>
-                  Analytics
+                                    Analytics
+                </ListItemText>
+              </ListItemButton>
+            </ListItem>
+            <ListItem key={'Help'} disablePadding>
+              <ListItemButton>
+                <ListItemIcon>
+                  <HelpIcon/>
+                </ListItemIcon>
+                <ListItemText primary={'Help'}>
+                                    Help
                 </ListItemText>
               </ListItemButton>
             </ListItem>
           </List>
-          <Divider />
+          <Divider/>
         </Box>
       </Drawer>
     </>
   )
 }
 
-function InterpreterOutput () {
+interface InterpreterOutputProps {
+    running: boolean;
+    output: string[];
+}
+
+function InterpreterOutput(props: InterpreterOutputProps) {
   return (
-    <Box sx={{border: '1px solid #000'}}>
-      <pre>HELLO WORLD</pre>
-    </Box>
+    <>
+      {props.running && <LinearProgress/>}
+      <Box sx={{padding: 1}}>
+        <pre>
+          {props.output.join("\n")}
+
+        </pre>
+      </Box>
+    </>
+  )
+}
+
+function Interpreter() {
+  const [code, setCode] = useState<string>("")
+  const [running, setRunning] = useState<boolean>(false)
+  const [output, setOutput] = useState<string[]>([])
+
+  return (
+    <Grid container spacing={2}>
+      <Grid item xs={12}>
+        <TextField
+          id="code"
+          label={`Code ${code.length ? `(${code.length} bytes)` : ''}`}
+          multiline
+          value={code}
+          onChange={(el) => setCode(el.target.value)}
+          maxRows={Infinity}
+          variant="outlined"
+          sx={{display: "flex"}}
+          InputProps={{style: {fontFamily: "monospace"}}}
+        />
+      </Grid>
+      <Grid item xs={12}>
+        <TextField
+          id="inputs"
+          label="Input Arguments"
+          variant="outlined"
+          multiline
+          maxRows={Infinity}
+          sx={{display: "flex"}}
+          InputProps={{style: {fontFamily: "monospace"}}}
+        />
+      </Grid>
+      <Grid item xs={12}>
+        <Button
+          variant='contained'
+          sx={{marginRight: 2}}
+          onClick={() => {
+            setRunning(!running)
+
+            if (!running) {
+              setOutput([])
+              setTimeout(() => {
+                setOutput(["OUTPUT"])
+                setRunning(false)
+              }, 1000)
+            }
+          }}
+          startIcon={running ? <StopIcon/> : <PlayArrowIcon/>}
+        >
+          {
+            running ? "Cancel" : "Run"
+          }
+        </Button>
+        <Button variant='outlined' startIcon={<ShareIcon/>}>Share</Button>
+      </Grid>
+      <Grid item xs={12}>
+        <InterpreterOutput running={running} output={output}/>
+      </Grid>
+    </Grid>
   )
 }
 
 function App() {
-  const [code, setCode] = useState<string>("10".repeat(1000))
-  const [running, setRunning] = useState<boolean>(true)
-
-  // <div style={{ textAlign: "right", display: "block" }}>({code.length} characters)</div>-->
   return (
     <Box sx={{display: 'flex'}}>
       <ButtonAppBar/>
-      <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
-        <Toolbar />
-        <Grid container spacing={2}>
-          <Grid item xs={12}>
-            <TextField
-              id="code"
-              label={`Code ${code.length ? `(${code.length} bytes)` : ''}`}
-              multiline
-              value={code}
-              onChange={(el) => setCode(el.target.value) }
-              maxRows={Infinity}
-              variant="outlined"
-              sx={{ display: "flex"}}
-              InputProps={{style: { fontFamily: "monospace"}}}
-            />
-          </Grid>
-          <Grid item xs={12}>
-            <TextField
-              id="inputs"
-              label="Input Arguments"
-              variant="outlined"
-              multiline
-              maxRows={Infinity}
-              sx={{ display: "flex" }}
-              InputProps={{style: { fontFamily: "monospace"}}}
-            />
-          </Grid>
-          <Grid item xs={12}>
-            <Button variant='contained' sx={{marginRight:2}} onClick={() => setRunning(!running) }>
-              {
-                running ?
-                  <div>Run
-                    <CircularProgress sx={{marginLeft: 2}} size={15} color="inherit"/>
-                  </div>
-                  : <div>Run</div>
-              }
-            </Button>
-            <Button variant='outlined' startIcon={<ShareIcon/>}>Share</Button>
-          </Grid>
-          <Grid item xs={12}>
-            <InterpreterOutput/>
-          </Grid>
-        </Grid>
+      <Box component="main" sx={{flexGrow: 1, p: 3}}>
+        <Toolbar/>
+        <Interpreter/>
       </Box>
     </Box>
   )
